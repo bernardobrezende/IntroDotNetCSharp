422b01f51ab934f84a5e1238ec66acdb9c7472ac
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-06-19 10:39:46 -0300

    ADD: Adding HelloWorld.Web application.

---
+++ src/Day-1/HelloWorld.Web/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.UI;
+
+namespace HelloWorld.Web
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.myH2.InnerText = "Hello World by C# code!";
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            this.myH2.InnerText = "Button Clicked!";
+            //
+            this.showInputText.InnerText = this.TextBox1.Text;
+        }
+    }
+}
