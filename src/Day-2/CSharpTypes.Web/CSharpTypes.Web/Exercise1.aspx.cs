36a696da48c9ee544ca2042bb28de0e4db747caa
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-06-20 10:05:09 -0300

    ADD: Adding Exercise 1 to Day-2.

---
+++ src/Day-2/CSharpTypes.Web/CSharpTypes.Web/Exercise1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSharpTypes.Web
+{
+    public partial class Exercise1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            decimal value1 = Decimal.Parse(this.TextBox1.Text);
+            decimal value2 = Decimal.Parse(this.TextBox2.Text);
+            //
+            Money total = new Money(value1 + value2);
+            //
+            this.Label1.Text = total.ToString();
+        }
+    }
+}
