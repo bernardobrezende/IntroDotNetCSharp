42b8d0c9902cbfbd39c7b7ecd732db397955deea
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-06-20 13:36:45 -0300

    ADD: Adding source codes for Day-2.

---
+++ src/Day-2/CSharpTypes.Web/CSharpTypes.Web/Exercise2.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSharpTypes.Web
+{
+    public partial class Exercise2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnUSD_Click(object sender, EventArgs e)
+        {
+            decimal usdValue = Decimal.Parse(this.TextBox1.Text);
+            MoneyForExercise2 usd = new MoneyForExercise2(usdValue, Currency.USD);
+            //
+            this.lblFormattedMoney.Text = usd.ToString();
+        }
+
+        protected void btnBRL_Click(object sender, EventArgs e)
+        {
+            decimal brlValue = Decimal.Parse(this.TextBox1.Text);
+            MoneyForExercise2 brl = new MoneyForExercise2(brlValue, Currency.BRL);
+            //
+            this.lblFormattedMoney.Text = brl.ToString();
+        }
+    }
+}
---
+++ src/Day-2/CSharpTypes.Web/CSharpTypes.Web/MoneyForExercise2.cs
@@ -0,0 +1,46 @@
+﻿namespace CSharpTypes.Web
+{
+    public struct MoneyForExercise2
+    {
+        // Private is the default accessor
+        private decimal value;
+        private Currency currency;
+
+        private const decimal MaxValue = 1000M;
+        private readonly decimal MinValue;
+
+        /// <summary>
+        /// Represents a money.
+        /// </summary>
+        /// <param name="value">Value for the money.</param>
+        public MoneyForExercise2(decimal value, Currency currency)
+        {
+            this.value = value;
+            this.MinValue = 6;
+            //
+            this.currency = currency;
+        }
+
+        public override string ToString()
+        {
+            if (this.currency == Currency.USD)
+                return "U$ " + this.value.ToString();
+            else
+                return "R$ " + this.value.ToString();                
+        }
+
+        /// <summary>
+        /// Factory Method.
+        /// </summary>
+        /// <returns></returns>
+        public static MoneyForExercise2 Thousand()
+        {
+            return new MoneyForExercise2(MaxValue, Currency.BRL);
+        }
+
+        public MoneyForExercise2 One()
+        {
+            return new MoneyForExercise2(1, Currency.BRL);
+        }
+    }
+}
