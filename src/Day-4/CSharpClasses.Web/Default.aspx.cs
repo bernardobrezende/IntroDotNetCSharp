781ed9ad15c2d8539a00455f4a56d5a81c4c8805
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-06-22 10:22:03 -0300

    ADD: Adding source codes from Day-4 (C# classes).

---
+++ src/Day-4/CSharpClasses.Web/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharpClasses.Web.MyClasses;
+
+namespace CSharpClasses.Web
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Creating with optional parameters
+            Beer b3 = new Beer("Therezópolis", "Brasil");
+            Beer b4 = new Beer("Coruja");
+
+            try
+            {
+                Beer b5 = new Beer(null, "RS");
+                b5.Open();
+            }
+            catch { } // Never do this!!
+
+            // Creating using type-initializer
+            Beer b6 = new Beer("Coruja")
+            {
+                Weight = 456,
+            };
+            b6.Name = "";
+            // Java Style:
+            //b5.setWeight(456);
+        }
+
+        protected void btnOpenAndDrink_Click(object sender, EventArgs e)
+        {
+            // Creating beer
+            Beer createdBeer = new Beer(this.txtName.Text
+                , "Brasil"
+                , Convert.ToDouble(this.txtInitialWeight.Text));            
+            // Opening beer
+            createdBeer.Open();
+            // Drinking the beer
+            double w = Convert.ToDouble(this.txtWeight.Text);
+            createdBeer.Drink(weight: w);
+
+            // Consuming IEnumerable of errors
+            //IEnumerable<string> errors = createdBeer.DrinkAndGetErrors(w);
+
+            #region Displaying errors
+
+            if (createdBeer.Errors.Count > 0)
+            {
+                StringBuilder errorBuilder = new StringBuilder();
+                foreach (string error in createdBeer.Errors)
+                {
+                    errorBuilder.AppendLine(error);
+                }
+                this.lblErrors.Visible = true;
+                this.lblErrors.Text = errorBuilder.ToString();
+            }
+            else
+            {
+                this.lblResult.Visible = true;
+                this.lblResult.Text = "Drank with success! You can still drink more " + createdBeer.Weight + " ml";
+            }            
+
+            #endregion
+        }
+    }
+}
---
+++ src/Day-4/CSharpClasses.Web/MyClasses/Beer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharpClasses.Web.MyClasses
+{
+    public class Beer
+    {
+        #region Fields
+
+        private string name;
+        private string country;
+
+        #endregion
+
+        #region Properties
+
+        // Properties canonical form
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        // Properties shortcut style (new in C# 3)
+        public string Country { get; private set; }
+        public double Weight { get; set; }
+        // Flag
+        private Boolean IsOpened { get; set; }
+        public double InitialWeight { get; private set; }
+        public IList<string> Errors { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Beer(string beerName, string country = default(string), double initialWeight = default(double))
+        {
+            if (String.IsNullOrEmpty(beerName))
+                throw new ArgumentException("Beer name is required.");
+            
+            this.name = beerName;
+            // Null coalsing and ternary operator samples
+            this.country = country ?? "Brasil";
+            this.country = country != null ? country : "Brasil";
+            this.Errors = new List<string>();
+            this.Weight = this.InitialWeight = initialWeight;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Open()
+        {
+            this.IsOpened = true;
+        }
+
+        public void Drink(double weight)
+        {
+            if (!this.IsOpened)
+                //throw new InvalidOperationException("Beer must be opened!");
+                this.Errors.Add("Beer must be opened!");
+            else if (weight > this.InitialWeight)
+                this.Errors.Add("Cannot drink too much!");
+            // 
+            this.Weight -= weight;
+        }
+
+        public IEnumerable<string> DrinkAndGetErrors(double weight)
+        {
+            if (!this.IsOpened)
+                yield return "Beer must be opened!";
+            if (weight > this.InitialWeight)
+                yield return "Cannot drink too much!";
+            //
+            this.Weight -= weight;
+        }
+
+        #endregion        
+    }
+}
