1aa5cabdccf4bc213139c56811ff7990c362fb80
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2012-06-27 20:13:03 -0300

    ADD: Adding Entity Framework Container samples.

--- src/Day-8/CSharpEntityFramework.Web/CSharpEntityFramework.Web/Default.aspx.cs
+++ src/Day-8/CSharpEntityFramework.Web/CSharpEntityFramework.Web/Default.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using CSharpEntityFramework.Web.Models;
 
 namespace CSharpEntityFramework.Web
@@ -7,7 +8,64 @@ namespace CSharpEntityFramework.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Beer b = new PremiumBeer();
+            if (!IsPostBack)
+            {
+                Beer b = new PremiumBeer();
+                b.Name = "Primator";
+                b.Weight = b.InitialWeight = 645;
+                b.Country = "CZECH";
+
+
+                using (TweetBeerContainer dbContainer = new TweetBeerContainer())
+                {
+                    #region Inserting
+
+                    dbContainer.AddToBeer(b);
+                    dbContainer.SaveChanges();
+
+                    #endregion
+
+                    #region Updating
+
+                    b.Name = "Corona";
+                    dbContainer.SaveChanges();
+
+                    #endregion
+
+                    #region Delete
+
+                    //dbContainer.DeleteObject(b);
+                    //dbContainer.SaveChanges();
+
+                    #endregion
+
+                    #region LINQ Query
+
+                    var coronas = from beer in dbContainer.Beer.ToList()
+                                  where beer.Name == "Corona"
+                                  select beer;                                  
+
+                    #endregion
+                }
+
+                #region Equivalent to using
+
+                //TweetBeerContainer v = null;
+                //try
+                //{
+                //    v = new TweetBeerContainer();
+                //    // Fazer um monte de coisa
+                //}
+                //catch
+                //{
+                //}
+                //finally
+                //{
+                //    v.Dispose();
+                //}
+
+                #endregion
+            }
         }
     }
 }
