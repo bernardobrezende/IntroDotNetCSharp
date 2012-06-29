88920e155853e30766236f054b674c831d5a1bba
Bernardo B Rezende <bernardobrezende@gmail.com>
2012-06-29 00:18:39 -0300

    ADD: Adding Twitter API initial structure.

---
+++ src/Day-10/TweetBeer.Web/TweetBeer.Web/Twitter/BeerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using TweetBeer.Web.Models;
+using TweetSharp;
+
+namespace TweetBeer.Web.Twitter
+{
+    public static class BeerExtensions
+    {
+        public static void Tweet(this Beer beer, string message)
+        {
+            if (message.Length > 140)
+                throw new ArgumentOutOfRangeException("Tweet must have at maximum 140 characters.");
+
+            TwitterService tw = new TwitterService(
+                ConfigurationManager.AppSettings["TwitterConsumerKey"],
+                ConfigurationManager.AppSettings["TwitterConsumerSecret"]);
+
+            var requestToken = tw.GetRequestToken();
+
+            tw.AuthenticateWith(
+                ConfigurationManager.AppSettings["TwitterToken"],
+                ConfigurationManager.AppSettings["TwitterTokenSecret"]);
+
+            TwitterStatus ts = tw.SendTweet(message);
+        }
+    }
+}
